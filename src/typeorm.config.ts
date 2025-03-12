05d41a9dc434bd5840d5a0b49cc9d238f019c13c
Илья <tiufiakovilia255465@gmail.com>
2025-03-12 19:55:10 +0300

    добавил подключение к бд

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -2,8 +2,8 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
+    host: '94.26.239.216',//'localhost',
+    port: 35264,//5432,
     username: 'postgres',
     password: '1234',
     database: 'test',
